343c59ff9a7e143190a4a58edb0a3b9ed94cbcf6
Muhammad Essa <thisismuhammadessa@gmail.com>
2026-08-21 22:05:07 +0500

    Update Terraform version and configure cloud settings
    Updated Terraform required version and added cloud configuration.

--- Terraform/backend.tf
+++ Terraform/backend.tf
@@ -1,5 +1,6 @@
 terraform {
-  required_version = "~> 1.13.3"
+  required_version = "1.15.9"
+
   required_providers {
     aws = {
       source  = "hashicorp/aws"
@@ -7,8 +8,15 @@ terraform {
     }
   }
 
-  cloud {}
+  cloud {
+    organization = "devhub"
+
+    workspaces {
+      name = "Netflix-Clone-Project"
+    }
+  }
 }
+
 provider "aws" {
   region = var.aws-region
 }
